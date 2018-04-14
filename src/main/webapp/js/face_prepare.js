8bcb4fb5b5a4d8e188e9f4d29d247cf03de125bc
rigel <rigel.ferreira@hotmail.com>
2018-04-14 12:04:05 -0300

    facebook app version

--- src/main/webapp/js/face_prepare.js
+++ src/main/webapp/js/face_prepare.js
@@ -1,8 +1,11 @@
+// quando a versao da aplicação no face era 2.5 aqui tava 2.3
+// agora a versao no face ta v2.6 e aqui teve que ser 2.4
+// 13/04/2018
 window.fbAsyncInit = function() {
 FB.init({
   appId      : '432866086802252',
   xfbml      : true,
-  version    : 'v2.3'
+  version    : 'v2.4'
 });
 };
 
